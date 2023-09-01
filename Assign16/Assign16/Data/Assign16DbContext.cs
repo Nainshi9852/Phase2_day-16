0da0a7ee3e246065422f50f8a9753b19b150a0a9
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-09-01 16:06:49 +0530

    Initial Commit

---
+++ Assign16/Assign16/Data/Assign16DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assign16.Models;
+
+namespace Assign16.Data
+{
+    public class Assign16DbContext : DbContext
+    {
+        public Assign16DbContext (DbContextOptions<Assign16DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assign16.Models.Task> Task { get; set; } = default!;
+    }
+}
---
+++ Assign16/Assign16/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Assign16.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Assign16DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Assign16DbContext") ?? throw new InvalidOperationException("Connection string 'Assign16DbContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
